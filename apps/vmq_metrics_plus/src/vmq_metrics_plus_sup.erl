0bdbc9339d2fecb9926d8a968ca87b87658d8fca
Vivek Pipaliya <vivek.pipaliya@gojek.com>
2024-04-12 11:26:42 +0530

    chore: remove unnecessary comments

--- apps/vmq_metrics_plus/src/vmq_metrics_plus_sup.erl
+++ apps/vmq_metrics_plus/src/vmq_metrics_plus_sup.erl
@@ -19,14 +19,5 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
     {ok, {{one_for_one, 5, 10}, [?CHILD(vmq_metrics_plus, worker)]}}.
