b0ad5f15f92bf22b51d5c2aeea88e0472bd3dbbb
dhruvjain99 <dhruv.jain93@gmail.com>
2024-05-07 17:09:32 +0530

    chore: instrument plugin call until redis call

--- apps/vmq_generic_offline_msg_store/src/engines/vmq_offline_storage_engine_redis.erl
+++ apps/vmq_generic_offline_msg_store/src/engines/vmq_offline_storage_engine_redis.erl
@@ -1,10 +1,10 @@
 -module(vmq_offline_storage_engine_redis).
 
--export([open/1, close/1, write/5, delete/3, delete/4, read/4, find/3]).
+-export([open/1, close/1, write/6, delete/4, delete/5, read/5, find/4]).
 
 -include_lib("vmq_server/src/vmq_server.hrl").
 
--dialyzer([{nowarn_function, [write/5, delete/3, delete/4, read/4, find/3]}]).
+-dialyzer([{nowarn_function, [write/6, delete/4, delete/5, read/5, find/4]}]).
 
 % API
 open(Opts) ->
@@ -28,19 +28,34 @@ open(Opts) ->
                    {database, Database}],
     eredis:start_link(ConnectOpts).
 
-write(Client, SIdB, _MsgRef, MsgB, Timeout) ->
+write(Client, SIdB, _MsgRef, MsgB, Ts1, Timeout) ->
+    Ts2 = vmq_util:ts(),
+    Val = Ts2 - Ts1,
+    vmq_metrics:pretimed_measurement({vmq_offline_msg_store, write_queue}, Val),
     vmq_redis:query(Client, ["RPUSH", SIdB, MsgB], ?RPUSH, ?MSG_STORE_WRITE, Timeout).
 
-delete(Client, SIdB, Timeout) ->
+delete(Client, SIdB, Ts1, Timeout) ->
+    Ts2 = vmq_util:ts(),
+    Val = Ts2 - Ts1,
+    vmq_metrics:pretimed_measurement({vmq_offline_msg_store, delete_all_queue}, Val),
     vmq_redis:query(Client, ["DEL", SIdB], ?DEL, ?MSG_STORE_DELETE, Timeout).
 
-delete(Client, SIdB, _MsgRef, Timeout) ->
+delete(Client, SIdB, _MsgRef, Ts1, Timeout) ->
+    Ts2 = vmq_util:ts(),
+    Val = Ts2 - Ts1,
+    vmq_metrics:pretimed_measurement({vmq_offline_msg_store, delete_queue}, Val),
     vmq_redis:query(Client, ["LPOP", SIdB, 1], ?LPOP, ?MSG_STORE_DELETE, Timeout).
 
-read(_Client, _SIdB, _MsgRef, _Timeout) ->
+read(_Client, _SIdB, _MsgRef, Ts1, _Timeout) ->
+    Ts2 = vmq_util:ts(),
+    Val = Ts2 - Ts1,
+    vmq_metrics:pretimed_measurement({vmq_offline_msg_store, read_queue}, Val),
     {error, not_supported}.
 
-find(Client, SIdB, Timeout) ->
+find(Client, SIdB, Ts1, Timeout) ->
+    Ts2 = vmq_util:ts(),
+    Val = Ts2 - Ts1,
+    vmq_metrics:pretimed_measurement({vmq_offline_msg_store, find_queue}, Val),
     case vmq_redis:query(Client, ["LRANGE", SIdB, "0", "-1"], ?FIND, ?MSG_STORE_FIND, Timeout) of
         {ok, MsgsInB} ->
             DMsgs = lists:foldr(fun(MsgB, Acc) ->
